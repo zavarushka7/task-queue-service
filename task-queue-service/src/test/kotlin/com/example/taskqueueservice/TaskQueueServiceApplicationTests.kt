ba33b27cffce7254c884fe481f060a982633ef7e
zavarushka7 <zavarushka7@gmail.com>
2026-05-16 13:58:36 +0500

    init: базовая структура проекта

---
+++ task-queue-service/src/test/kotlin/com/example/taskqueueservice/TaskQueueServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.taskqueueservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskQueueServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
